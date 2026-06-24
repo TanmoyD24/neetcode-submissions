63481867047b98e8a90c4d7a035419761c36c325
Tanmoy Das <dastanmoy200224@gmail.com>
2026-06-24 12:37:19 +0530

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low = 1
+        high = max(piles)
+        n = len(piles)
+
+        def caneat(piles: List[int], mid: int) -> int:
+            count = 0
+            for i in range(n):
+                count += (piles[i] + mid - 1) // mid
+            return count
+
+        if (n == h):
+            return high
+
+        while(low <= high):
+            mid = low + (high - low) // 2
+            x = caneat(piles, mid)
+            if(x > h):
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low
